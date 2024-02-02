a4e4d485c26ae49856a31d1931f08f41be1475a3
Maria Camila Ortega <mcamilaortega12@gmail.com>
2024-02-02 22:28:39 +0000

    done with issues

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,32 +1,122 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
-		store: {
-			contacts:[
-				{
-				  "full_name": "Maria Ortega",
-				  "phone": 1234567890,
-				  "email": "maria.ortega@gmail.com",
-				  "address": "123 Main St."
-				},
-				{
-				  "full_name": "John Smith",
-				  "phone": 9876543210,
-				  "email": "john.smith@example.com",
-				  "address": "456 Oak Avenue"
-				},
-				{
-				  "full_name": "Alice Johnson",
-				  "phone": 5551234567,
-				  "email": "alice.johnson@mail.com",
-				  "address": "789 Elm Street"
-				}
-			  ]
+		store: {			
+			contacts: [],			
 		},
 		actions: {
-			getContacts:()=>{},
-			addContacts:()=>{},
-			deleteContacts:()=>{},
-			editContacts:()=>{}
+			// Use getActions to call a function within a fuction		
+			getContacts: () => {
+				const url = "";
+				fetch(url, {
+					method: "Get"					
+				})
+				.then(resp => {
+					console.log(resp.ok); // will be true if the response is successfull
+					console.log(resp.status); // the status code = 200 or code = 400 etc.
+					//console.log(resp.text()); // will try return the exact result as string
+					return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+				})
+				.then(data => {
+					setStore({contacts: data});
+					
+				})
+				.catch(error => {
+					//error handling
+					console.log(error);
+				})
+			},
+			addContact: (contact, navigate) => {				
+				const url = "https://playground.4geeks.com/apis/fake/contact/agenda/maria_ortega";
+				 fetch(url, {
+					method: "Post",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({						
+						"full_name": contact.full_name,
+                      	"email": contact.email,
+                      	"agenda_slug": "maria_ortega",
+                      	"address": contact.address,
+                      	"phone": contact.phone
+					})					
+				})
+				.then(resp => {
+					console.log(resp.ok); // will be true if the response is successfull
+					console.log(resp.status); // the status code = 200 or code = 400 etc.					
+					return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+				})
+				.then(data => {					
+					getActions().getContacts();
+					
+				})
+				.catch(error => {
+					//error handling
+					console.log(error);
+				})								
+				navigate("/");				
+			},
+			editContact:  (contact,navigate) => {
+				let store = getStore();
+				const url = `https://playground.4geeks.com/apis/fake/contact/${contact.id}`;
+				fetch(url, {
+					method: "Put",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({						
+						"full_name": contact.full_name,
+                      	"email": contact.email,
+                      	"agenda_slug": "maria_ortega",
+                      	"address": contact.address,
+                      	"phone": contact.phone
+					})					
+				})
+				.then(resp => {
+					console.log(resp.ok); // will be true if the response is successfull
+					console.log(resp.status); // the status code = 200 or code = 400 etc.
+					//console.log(resp.text()); // will try return the exact result as string
+					return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+				})
+				.then(data => {
+					//console.log(data);
+					let updatedContact = store.contacts.find((item) => {
+						return item.id == contact.id;
+
+					});
+					updatedContact.full_name = contact.full_name;
+					updatedContact.email = contact.email;
+					updatedContact.address = contact.address;
+					updatedContact.phone = contact.phone;
+					let newContact = [...store.contacts];
+					setStore({contacts: newContact});
+					
+				})
+				.catch(error => {
+					//error handling
+					console.log(error);
+				})						
+				navigate("/");
+			},
+			deleteContact: (contact) => {
+				let store = getStore();
+				const url = `https://playground.4geeks.com/apis/fake/contact/${contact}`;
+				fetch(url, {
+					method: "Delete"					
+				})
+				.then(resp => {
+					console.log(resp.ok); // will be true if the response is successfull
+					console.log(resp.status); // the status code = 200 or code = 400 etc.
+				})
+				.then(data => {					
+					let val = store.contacts.filter((item) => item.id != contact);
+					setStore({contacts: val});
+				})
+				.catch(error => {
+					//error handling
+					console.log(error);
+				})											
+				
+			}			
 		}
 	};
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,19 +1,33 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import { ContactCard } from "../component/contact card";
+import { ContactCard } from "../component/ContactCard";
 import "../../styles/home.css";
+import { Link } from "react-router-dom"
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
   return (
-    <div className="">
+    <div className="container justify-content-center">
+
+      <div className="justify-content-right">
+       <Link to = "/addcontact"> 
+       <button className="btn btn-success">
+        Add Contact
+      </button>
+       </Link>
+
+      </div>
+      <div>
       {store.contacts.map((item, index) => {
         return (
           <div key={index}>
-            <ContactCard />
+            <ContactCard contact={item} index={index}/>
           </div>
         );
       })}
+
+      </div>
+     
     </div>
   );
 };
